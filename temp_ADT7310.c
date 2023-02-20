93c2739e9adb0e81b08e8e603485321d9d46e39c
Thompie2000 <75269299+Thompie2000@users.noreply.github.com>
2023-02-20 23:16:09 +0100

    Create temp_ADT7310.c

---
+++ temp_ADT7310.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define TEMP_SCLK   23
+#define TEMP_CS     26
+#define TEMP_MOSI   19
+#define TEMP_MISO   21
+
+// initialisate the temperature sensor
+void init_temp()
+{
+
+}
+
+int main()
+{
+
+}
