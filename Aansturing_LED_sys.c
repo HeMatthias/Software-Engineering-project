c05c533c0c341606f1f66bb24b618eebf244fd58
Hendrickx Matthias <he.matthias@gmail.com>
2023-05-11 14:18:58 +0200

    Update Aansturing_LED_sys.c

--- Aansturing_LED_sys.c
+++ Aansturing_LED_sys.c
@@ -108,3 +108,222 @@ void Led_D1_Reset(){
 
     close(fd);
 }
+
+// Vanaf hier een nieuwe c code gemaakt
+
+/*
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// Define the pins for each LED
+#define LED_D1 17
+#define LED_D2 18
+#define LED_D3 27
+#define LED_D4 22
+#define LED_D5 23
+#define LED_D6 24
+#define LED_D7 14
+
+// Function prototypes
+void Init_Led(int led_pin);
+void Led_On(int led_pin);
+void Led_Off(int led_pin);
+void Led_Reset(int led_pin);
+void Knight_Rider();
+
+int main()
+{
+    // Initialize all LEDs
+    Init_Led(LED_D1);
+    Init_Led(LED_D2);
+    Init_Led(LED_D3);
+    Init_Led(LED_D4);
+    Init_Led(LED_D5);
+    Init_Led(LED_D6);
+    Init_Led(LED_D7);
+
+    // Toggle each LED 50 ms on, 50ms off, 100 times (10 seconds)
+    for (int i = 0; i < 50; i++) {
+        Led_On(LED_D1);
+        Led_On(LED_D2);
+        Led_On(LED_D3);
+        Led_On(LED_D4);
+        Led_On(LED_D5);
+        Led_On(LED_D6);
+        Led_On(LED_D7);
+        usleep(50000);
+
+        Led_Off(LED_D1);
+        Led_Off(LED_D2);
+        Led_Off(LED_D3);
+        Led_Off(LED_D4);
+        Led_Off(LED_D5);
+        Led_Off(LED_D6);
+        Led_Off(LED_D7);
+        usleep(50000);
+    }
+
+    Knight_Rider();
+
+    // Reset all LEDs
+    Led_Reset(LED_D1);
+    Led_Reset(LED_D2);
+    Led_Reset(LED_D3);
+    Led_Reset(LED_D4);
+    Led_Reset(LED_D5);
+    Led_Reset(LED_D6);
+    Led_Reset(LED_D7);
+
+    return 0;
+}
+
+void Init_Led(int led_pin){
+    // Export the GPIO pin
+    int fd = open("/sys/class/gpio/export", O_WRONLY);
+    if (fd == -1) {
+        perror("Unable to open /sys/class/gpio/export");
+        exit(1);
+    }
+
+    char buffer[4];
+    int len = snprintf(buffer, sizeof(buffer), "%d", led_pin);
+    if (write(fd, buffer, len) != len) {
+        perror("Error writing to /sys/class/gpio/export");
+        exit(1);
+    }
+
+    close(fd);
+
+    // Set the pin to be an output by writing "out" to /sys/class/gpio/gpioXX/direction
+    char path[40];
+    snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/direction", led_pin);
+
+    fd = open(path, O_WRONLY);
+    if (fd == -1) {
+        perror("Unable to open /sys/class/gpio/gpioXX/direction");
+        exit(1);
+    }
+
+    if (write(fd, "out", 3) != 3) {
+        perror("Error writing to /sys/class/gpio/gpioXX/direction");
+        exit(1);
+    }
+
+    close(fd);
+}
+
+void Led_On(int led_pin){
+    // Write "1" to /sys/class/gpio/gpioXX/value to turn on the LED
+    char path[35];
+    snprintf(path, sizeof(path), "/sys/class/gpio/gpio%d/value", led_pin);
+
+    int fd = open(path, O_WRONLY);
+    if (fd == -1) {
+        perror("Unable to open /sys/class/gpio/gpioXX/value");
+        exit(1);
+    }
+
+    if (write(fd, "1", 1) != 1) {
+        perror("Error writing to gpio value file");
+        exit(1);
+    }
+
+    close(fd);
+}
+
+void Led_Off(int led_pin) {
+    char path[50];
+    sprintf(path, "/sys/class/gpio/gpio%d/value", led_pin);
+    int fd = open(path, O_WRONLY);
+    if (fd == -1) {
+        perror("Unable to open gpio value file");
+        exit(1);
+    }
+
+    if (write(fd, "0", 1) != 1) {
+        perror("Error writing to gpio value file");
+        exit(1);
+    }
+
+    close(fd);
+}
+
+void Led_Reset(int led_pin) {
+    int fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    if (fd == -1) {
+        perror("Unable to open /sys/class/gpio/unexport");
+        exit(1);
+    }
+
+    char pin_str[4];
+    sprintf(pin_str, "%d", led_pin);
+    if (write(fd, pin_str, strlen(pin_str)) != strlen(pin_str)) {
+        perror("Error writing to /sys/class/gpio/unexport");
+        exit(1);
+    }
+
+    close(fd);
+}
+
+void Knight_Rider()
+{
+    int delay_time = 100000; // Delay in microseconds (100ms)
+
+    for(int i = 0; i < 2; i++)
+    {
+        Led_On(LED_D1);
+        usleep(delay_time);
+        Led_Off(LED_D1);
+
+        Led_On(LED_D2);
+        usleep(delay_time);
+        Led_Off(LED_D2);
+
+        Led_On(LED_D3);
+        usleep(delay_time);
+        Led_Off(LED_D3);
+
+        Led_On(LED_D4);
+        usleep(delay_time);
+        Led_Off(LED_D4);
+
+        Led_On(LED_D5);
+        usleep(delay_time);
+        Led_Off(LED_D5);
+
+        Led_On(LED_D6);
+        usleep(delay_time);
+        Led_Off(LED_D6);
+
+        Led_On(LED_D7);
+        usleep(delay_time);
+        Led_Off(LED_D7);
+
+        Led_On(LED_D6);
+        usleep(delay_time);
+        Led_Off(LED_D6);
+
+        Led_On(LED_D5);
+        usleep(delay_time);
+        Led_Off(LED_D5);
+
+        Led_On(LED_D4);
+        usleep(delay_time);
+        Led_Off(LED_D4);
+
+        Led_On(LED_D3);
+        usleep(delay_time);
+        Led_Off(LED_D3);
+
+        Led_On(LED_D2);
+        usleep(delay_time);
+        Led_Off(LED_D2);
+    }
+}
+
+*/
